5dfa2048a725529e13a13c83bc81b4f39f6e30d0
Alan Edwardes <alan@alanedwardes.com>
2018-12-31 13:41:01 +0000

    Adds logging.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace DnsServerConsole
             MasterFile masterFile = new MasterFile();
             DnsServer server = new DnsServer(masterFile, "8.8.8.8");
 
+            server.Listening += (sender, e) => Console.WriteLine("Now listening on port 53");
+
             // Log every request
             server.Requested += (sender, e) => Console.WriteLine(e.Request);
             // On every successful request log the request and the response
@@ -18,7 +20,17 @@ namespace DnsServerConsole
             // Log errors
             server.Errored += (sender, e) => Console.WriteLine(e.Exception.Message);
 
-            server.Listen().GetAwaiter().GetResult();
+            Console.WriteLine("Starting server...");
+
+            try
+            {
+                server.Listen().GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
