40880e59a88b4aba6065cc7aa7894d0e6754d31c
Alan Edwardes <alan@alanedwardes.com>
2018-12-31 15:44:12 +0000

    Fixes use of reserved words.

--- Program.cs
+++ Program.cs
@@ -50,9 +50,9 @@ namespace DnsServerConsole
                 }, new Dictionary<string, AttributeValueUpdate>
                 {
                     { "Count", new AttributeValueUpdate { Action = "ADD", Value = new AttributeValue { N = "1" } } },
-                    { "Year", new AttributeValueUpdate { Action = "PUT", Value = new AttributeValue(DateTime.UtcNow.ToString("yyyy")) } },
-                    { "Month", new AttributeValueUpdate { Action = "PUT", Value = new AttributeValue(DateTime.UtcNow.ToString("yyyy-MM")) } },
-                    { "Date", new AttributeValueUpdate { Action = "PUT", Value = new AttributeValue(DateTime.UtcNow.ToString("yyyy-MM-dd")) } },
+                    { "CurrentYear", new AttributeValueUpdate { Action = "PUT", Value = new AttributeValue(DateTime.UtcNow.ToString("yyyy")) } },
+                    { "CurrentMonth", new AttributeValueUpdate { Action = "PUT", Value = new AttributeValue(DateTime.UtcNow.ToString("yyyy-MM")) } },
+                    { "CurrentDate", new AttributeValueUpdate { Action = "PUT", Value = new AttributeValue(DateTime.UtcNow.ToString("yyyy-MM-dd")) } },
                     { "Expiry", new AttributeValueUpdate { Action = "PUT", Value = new AttributeValue { N = DateTimeOffset.UtcNow.AddDays(30).ToUnixTimeSeconds().ToString() } } }
                 }));
             }
